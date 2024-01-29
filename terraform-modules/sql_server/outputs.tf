f65ce410d5bf73e2cd1959a16244e19d69b844f3
taylermoreland <taylermoreland@me.com>
2024-01-29 15:14:10 +0000

    fix

--- terraform-modules/sql_server/outputs.tf
+++ terraform-modules/sql_server/outputs.tf
@@ -1,7 +1,7 @@
 # sql_server/outputs.tf
 
-output "sql_server_fqdn" {
-  value = azurerm_sql_server.sql_server.fqdn
-}
+# output "sql_server_fqdn" {
+#   value = azurerm_sql_server.sql_server.fqdn
+# }
 
 # Additional outputs can go here...
